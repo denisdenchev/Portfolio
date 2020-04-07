27493ecfb58d44b9319a3b8b750612fddffb283b
Denis Denchev <Denis.Denchev@Nology.local>
2020-04-07 13:57:47 +0100

    playing with contact form

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
-// const sendEmail = () => {
-//   Email.send({
-//     Host: "smtp.yourisp.com",
-//     Username: "denisdenchevneosf@gmail.com",
-//     Password: "0F863E0D7C865647792803BA8DB1C2711340",
-//     To: "denisdenchevneosf@gmail.com",
-//     From: "you@isp.com",
-//     Subject: "This is the subject",
-//     Body: "And this is the body",
-//   }).then((message) => alert(message));
-// };
 
 window.onload = function () {
   document
@@ -22,6 +11,6 @@ window.onload = function () {
       // generate the contact number value
       this.contact_number.value = (Math.random() * 100000) | 0;
       emailjs.sendForm("contact_service", "contact_form", this);
-      alert("Send Successfully");
+      alert("Sent Successfully");
     });
 };
